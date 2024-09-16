1236a2417dd1e7ed2d61742b0fba2f89cc77fa91
miakoppen1 <mkoppen2@gmail.com>
2024-09-15 20:58:42 -0700

    8:58 PM

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = () => 
     {
-    return <div>Main Layout</div>
+    return <div>
+    <Header />
+    <Footer /> 
+    </div>
     }
+
 export default Layout
