6cdf64a467deaac1c9e602cc3ac786c08758a407
miakoppen1 <mkoppen2@gmail.com>
2024-09-17 19:56:47 -0700

    7:56 pm added comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,41 +5,41 @@ import { images } from "../constants";
 
 // the header will contain the logo and the ordered list of pages for the user to choose from !
 
-const Header = () => 
+const Header = () =>
     {
-    return ( 
+    return (
     <section>
         <header className="container mx-auto px-5 flex justify-between items-center">
             <div className="mt-3">
                 <img src={images.blahgLogo} alt="Logo" height="300" width="300" />
             </div>
-            <div className="flex gap-x-9 items-center"> 
+            <div className="flex gap-x-9 items-center">
                <ul className="flex gap-x-5 ">
-
+    {/* BUTTON 1: HOME BUTTON */}
                 <li className="bg--500 rounded-full relative">
                 <button className="border-2 border-black px-4 py-2 rounded-full text-black font-bold animated-button">
                     BLAHG Home
                 </button>
                 </li>
-
+    {/* BUTTON 2: FAQ BUTTON */}
                 <li>
                 <button className="border-2 border-black px-4 py-2 rounded-full text-black font-bold animated-button">
                     FAQ
                 </button>
                 </li>
-
+    {/* BUTTON 3: USER BUTTON */}
                 <li>
                 <button className="border-2 border-black px-4 py-2 rounded-full text-black font-bold animated-button">
                     Your BLAHG
                 </button>
                 </li>
-
+    {/* BUTTON 4: LOGIN BUTTON */}
                <li>
                 <button className="border-2 mr-4 border-black px-4 py-2 rounded-full text-black font-bold animated-button">
                     Login
                 </button>
                 </li>
-                
+
                 </ul>
             </div>
         </header>
