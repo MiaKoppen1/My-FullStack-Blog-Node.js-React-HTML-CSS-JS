6eb0a3e63c3ed32d867a6121a56756f2f9e307fc
miakoppen1 <mkoppen2@gmail.com>
2024-09-18 01:26:58 -0700

    9/18 1:26 AM Hero page created

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Hero from "./container/Hero";
 
 // this is where we create the layout for the homepage !
 
 const HomePage = () => 
     {
-    return <Layout></Layout> 
+    return <Layout>
+        <Hero />
+    </Layout> 
     };
 
     export default HomePage
---
+++ src/pages/container/Hero.jsx
@@ -0,0 +1,17 @@
+
+//Hero section of the homepage aka the media on the very front ...
+
+import React from 'react';
+
+
+const Hero = () =>
+     {
+    return (
+       <><div>
+            Hero
+        </div><div>
+                Hero
+            </div></>
+        )
+    }
+export default Hero
