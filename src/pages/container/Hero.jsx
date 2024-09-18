4ddb4bb76109e11cf3a5738e984d6bff4e2c6a65
miakoppen1 <mkoppen2@gmail.com>
2024-09-18 01:43:14 -0700

    1:42 AM finished enter user and password buttons

--- src/pages/container/Hero.jsx
+++ src/pages/container/Hero.jsx
@@ -14,8 +14,21 @@ const Hero = () =>
                 Expedita praesentium architecto fugiat recusandae aperiam quisquam dolorum quod suscipit ducimus!
                 Iusto ut, cumque nobis pariatur a voluptate! Hic earum itaque totam?
             </p>
-        </div><div>
+            </div>
+            <div>
                 Get Started!
+                <div>
+                Enter Username
+                  <div>
+                  <input type="text" />
+                  </div>
+                </div>
+                  <div>
+                Enter Password
+                  <div>
+                  <input type="text" />
+                </div>
+                  </div>
                 <button className="border-2 border-black px-4 py-2 rounded-full text-black font-bold animated-button">
                     Create Account
                 </button>
