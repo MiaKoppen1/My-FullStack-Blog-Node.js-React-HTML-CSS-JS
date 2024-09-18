b990d08661992b2c9ba3b890256a44859df648fd
miakoppen1 <mkoppen2@gmail.com>
2024-09-17 18:46:15 -0700

    6:46 PM buttons finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () =>
     {
     return ( 
     <section>
-        <header className="container mx-auto px-5 flex justify-between">
+        <header className="container mx-auto px-5 flex justify-between items-center">
             <div>
                 <img src={images.blogLogo} alt="Logo" height="180" width="180" />
             </div>
             <div className="flex gap-x-9 items-center"> 
-               <ul className="flex gap-x-5">
+               <ul className="flex gap-x-5 font-bold">
                 <li>
                 <button className="border-2 border-blue-950 px-6 py-2 rounded-full text-red-950 animated-button">
                     BLAHG Home
