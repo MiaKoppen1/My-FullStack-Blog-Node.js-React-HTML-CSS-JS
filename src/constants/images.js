7ab3b3faf770a30b65f538700b65393b439a0c9c
miakoppen1 <mkoppen2@gmail.com>
2024-09-18 02:28:33 -0700

    2:28 AM added a ton of images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,13 @@
 import blahgLogo from "../assets/blahgLogo.png";
+import img1 from "../assets/img1.jpg";
+import img2 from "../assets/img2.jpg";
+import img3 from "../assets/img3.jpg";
+import img5 from "../assets/img5.jpg";
+import img6 from "../assets/img6.jpg";
 
-const images = 
+const images =
     {
-    blahgLogo,
+    blahgLogo, img1, img2, img3, img5, img6,
     }
 
 export default images;
--- src/pages/container/Hero.jsx
+++ src/pages/container/Hero.jsx
@@ -2,7 +2,7 @@
 //Hero section of the homepage aka the media on the very front ...
 
 import React from 'react';
-
+import { images } from "../constants";
 
 const Hero = () =>
      {
@@ -32,6 +32,9 @@ const Hero = () =>
                 <button className="border-2 border-black px-4 py-2 rounded-full text-black font-bold animated-button">
                     Create Account
                 </button>
+                    <div>
+                        <img src={images.img1} alt="communication"></img>
+                    </div>
             </div></>
         )
     }
