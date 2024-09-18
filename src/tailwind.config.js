497feeb84b5bd453cdff84ef2bb0171df0dddf24
miakoppen1 <mkoppen2@gmail.com>
2024-09-17 20:20:44 -0700

    8:20 PM fonts added

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         }
       },
       fontFamily: {
-        opensans: [],
-        roboto: []
+        opensans: ["'Open Sans'", "sans-serif"],
+        roboto: ["'Roboto'", "sans-serif"]
       }
     },
   },
